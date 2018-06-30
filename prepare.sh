899f0152e663851813bcce551ec2fb62b363deb5
Bruce Cota <bcota@unicon.net>
2018-06-30 12:12:20 -0400

    quote file names for pictures with infernal white space in them

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir main
-mkdir thumb
+mkdir -p main
+mkdir -p thumb
 for i in *.JPG *.jpg; do
-    convert -thumbnail 150 $i thumb/$i
-    mv $i main/${i}
+    convert -thumbnail 150 "$i" "thumb/$i"
+    mv "$i" "main/${i}"
 done
